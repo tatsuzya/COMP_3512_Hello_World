1c79146187c888da5a8525373c4c6add403ba077
Jay Lin <clin188@my.bcit.ca>
2018-01-15 15:06:30 -0800

    I'm done

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -8,8 +8,7 @@
 // RETURN: the greatest common denominator of a and b.
 int gcd(const int a, const int b)
 {
-	// Replace this line with your code (have fun!).
-	return 0;
+	return b == 0 ? a : gcd(b, a%b);
 }
 
 // Fibonacci
@@ -18,6 +17,9 @@ int gcd(const int a, const int b)
 // RETURN: the nth positive integer in the Fibonacci sequence.
 long long fibonacci(const int n)
 {
-	// Replace this line with your code (challenge: do this with recursion!).
-	return 0;
+	if( (n == 0) || (n ==1))
+	{
+		return n;
+	}
+	return fibonacci(n - 1) + fibonacci(n - 2);
 }
